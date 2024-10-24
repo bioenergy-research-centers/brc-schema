acf49cd8ecb0faebf0b37609ec7e7045de0eb093
caufieldjh <j.harry.caufield@gmail.com>
2024-10-24 12:41:59 -0400

    Bump version to 0.0.5

--- src/brc_schema/datamodel/brc_schema.py
+++ src/brc_schema/datamodel/brc_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from brc_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-17T12:23:48
+# Generation date: 2024-10-24T12:41:41
 # Schema: brc_schema
 #
 # id: https://w3id.org/brc/brc_schema
@@ -26,7 +26,7 @@ from linkml_runtime.linkml_model.types import Boolean, Date, Integer, String, Ur
 from linkml_runtime.utils.metamodelcore import Bool, URI, URIorCURIE, XSDDate
 
 metamodel_version = "1.7.0"
-version = "0.0.4"
+version = "0.0.5"
 
 # Overwrite dataclasses _init_fn to add **kwargs in __init__
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
